8e1e42c0591b7647d8bb344a3c7471d522161697
Noel <noel.zeng@auckland.ac.nz>
2022-06-16 12:00:01 +1200

    Add class for app state

--- models.py
+++ models.py
@@ -1,16 +1,17 @@
-import yaml
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Type
 from dataclasses import dataclass, field
-
+import yaml
 from yaml.loader import Loader
-from yaml.nodes import MappingNode, ScalarNode, SequenceNode
+from yaml.nodes import Node
+import logging
 
 class YAMLSerializable(yaml.YAMLObject):
     @classmethod
-    def from_yaml(cls, loader: Loader, node) -> Any:
+    def from_yaml(cls : Type, loader: Loader, node: Node) -> Any:
         """
         Convert a representation node to a Python object,
         calling __init__ to create a new object.
+
         We're using dataclasses to create an __init__ method
         which sets default values for fields not present in YAML document.
         By default, YAMLObject does not call __init__, so yaml.safe_load throws an exception
@@ -24,6 +25,9 @@ class YAMLSerializable(yaml.YAMLObject):
 
 @dataclass
 class IAccessControl():
+    """
+    A class representing fields related to ACL controls.
+    """
     admin_groups: List[str] = field(default_factory=list)
     admin_users: List[str] = field(default_factory=list)
     read_groups: List[str] = field(default_factory=list)
@@ -35,13 +39,20 @@ class IAccessControl():
 
 @dataclass
 class IMetadata():
+    """
+    A class representing fields related to schema parameters.
+    """
     metadata: Dict[str, str] = field(default_factory=dict)
 
 
 @dataclass
 class Project(YAMLSerializable, IAccessControl, IMetadata):
+    """
+    A class representing MyTardis Project objects.
+    """
     yaml_tag = u'!Project'
     yaml_loader = yaml.SafeLoader
+    # yaml_dumper = yaml.SafeDumper
     project_name: str = ""
     project_id: str = ""
     alternate_ids: List[str] = field(default_factory=list)
@@ -50,8 +61,12 @@ class Project(YAMLSerializable, IAccessControl, IMetadata):
 
 @dataclass
 class Experiment(YAMLSerializable, IAccessControl, IMetadata):
+    """
+    A class representing MyTardis Experiment objects.
+    """
     yaml_tag = u'!Experiment'
     yaml_loader = yaml.SafeLoader
+    # yaml_dumper = yaml.SafeDumper
     experiment_name: str = ""
     project_id: str = ""
     experiment_id: str = ""
@@ -60,8 +75,12 @@ class Experiment(YAMLSerializable, IAccessControl, IMetadata):
 
 @dataclass
 class Dataset(YAMLSerializable, IAccessControl, IMetadata):
+    """
+    A class representing MyTardis Dataset objects.
+    """
     yaml_tag = u'!Dataset'
     yaml_loader = yaml.SafeLoader
+    # yaml_dumper = yaml.SafeDumper
     dataset_name: str = ""
     experiment_id: List[str] = field(default_factory=list)
     dataset_id: str = ""
@@ -69,17 +88,93 @@ class Dataset(YAMLSerializable, IAccessControl, IMetadata):
 
 @dataclass
 class FileInfo(YAMLSerializable, IAccessControl, IMetadata):
+    """
+    A class representing MyTardis Datafile objects.
+    """
     yaml_tag = u'!FileInfo'
+    # yaml_dumper = yaml.SafeDumper
     yaml_loader = yaml.SafeLoader
     name: str = ""
 
 
 @dataclass
 class Datafile(YAMLSerializable):
+    """
+    A class representing a set of MyTardis datafile objects.
+    """
     yaml_tag = u'!Datafile'
     yaml_loader = yaml.SafeLoader
+    # yaml_dumper = yaml.SafeDumper
     dataset_id: str = ""
     files: List[FileInfo] = field(default_factory=list)
+
+@dataclass
+class IngestionMetadata():
+    """
+    A class representing a collection of metadata, with
+    objects of different MyTardis types. It can be serialised
+    to become a YAML file for ingestion into MyTardis.
+    """
+    # A list of objects of each type.
+    projects : List[Project] = field(default_factory=list)
+    experiments : List[Experiment] = field(default_factory=list)
+    datasets : List[Dataset] = field(default_factory=list)
+    datafiles : List[Datafile] = field(default_factory=list)
+
+    def to_yaml(self):
+        """
+        Returns a string of the YAML representation of the metadata.
+        """
+        concatenated: List[Any] = self.projects
+        concatenated.extend(self.experiments)
+        concatenated.extend(self.datasets)
+        concatenated.extend(self.datafiles)
+        yaml_file = yaml.dump_all(concatenated)
+        return yaml_file
+
+    @staticmethod
+    def from_yaml(yaml_rep : str):
+        """Returns a IngestionMetadata object by loading metadata from content of a YAML file.
+        
+        Parameters
+        ----------
+        yaml_rep : str
+            The content of a YAML file. Note that this is the content, not the path of the file.
+            The function does not read from a file for you, you have to pass in the file's content.
+        """
+        metadata = IngestionMetadata()
+        objects = yaml.safe_load_all(yaml_rep)
+        # Iterate through all the objects,
+        # sorting them into the right list
+        # based on type.
+        for obj in objects:
+            if isinstance(obj, Project):
+                metadata.projects.append(obj)
+            elif isinstance(obj, Experiment):
+                metadata.experiments.append(obj)
+            elif isinstance(obj, Dataset):
+                metadata.datasets.append(obj)
+            elif isinstance(obj, Datafile):
+                metadata.datafiles.append(obj)
+            else:
+                logging.warning("Encountered unknown object while reading YAML" + 
+                                ", ignored. Object was %s", obj)
+        return metadata
+
+
+# # To use:
+# dataset = Dataset()
+# dataset.dataset_name = "Calibration 10 X"
+# dataset.dataset_id = "2022-06-calibration-10-x"
+
+# # To dump into YAML
+# yaml.dump(dataset)
+
+# # To dump multiple objects
+# datafile = Datafile()
+# datafile.dataset_id = "2022-06-calibration-10-x"
+# output = [dataset, datafile]
+# yaml.dump_all(output)
 
 # # If not working with tags, strip them out and process as normal.
 # import yaml
--- mytardismetadataeditor.py
+++ mytardismetadataeditor.py
@@ -2,14 +2,13 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWizard, QWizardPage, QVBoxLayout, QLabel
 from PyQt5 import uic
 from mainwindow import Ui_MainWindow
-from models import Project, Experiment, Dataset, Datafile, FileInfo
-
+from models import IngestionMetadata
 
 class MyTardisMetadataEditor(QMainWindow):
-    
     def __init__(self):
+        # Initialise the app ingestion metadata state.
+        self.metadata = IngestionMetadata()
         QMainWindow.__init__(self)
-
 
 if __name__ == "__main__":
     app = QApplication([])
