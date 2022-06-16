971fcb06a64324af542efc38a70bf8c02463aeba
Libby Li <xli677@wifi-staff-172-24-24-102.net.auckland.ac.nz>
2022-06-17 09:31:31 +1200

    enable to open wizard window from mainwindow

--- mytardismetadataeditor.py
+++ mytardismetadataeditor.py
@@ -1,17 +1,66 @@
+from fileinput import filename
+from os import fpathconf
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWizard, QWizardPage, QVBoxLayout, QLabel
-from PyQt5 import uic
-from mainwindow import Ui_MainWindow
-
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QWizard, QWizardPage, QVBoxLayout, QLabel,QFileDialog
+from PyQt5 import uic,QtWidgets,QtCore,QtGui
+# from mainwindow import Ui_MainWindow
 
 class MyTardisMetadataEditor(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super(QMainWindow, self).__init__()
 
+        # load the ui file
+        uic.loadUi('MainWindow.ui', self)
+
+        # define our widgets
+        self.actionImport_data_files = self.findChild(QAction, "actionImport_data_files")
+        self.actionImport_data_files.triggered.connect(self.openWindow)
+        self.show()
+
+    def openWindow(self):  
+        self.ui = WindowWizard()
+        self.ui.show()
+###        self.close()
+
+class WindowWizard(QWizard):
+    def __init__(self):
+        super(QWizard, self).__init__()
+        uic.loadUi('add-files-wizard.ui', self)
+
+        # define out widgets
+        self.datafilepushButton.clicked.connect(self.pushButton_handler)
+
+    def pushButton_handler(self):
+        self.open_dialog_box()
+
+    def fpath(self):
+        return self._fpath
+
+    def open_dialog_box(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        self._fpath = path
+'''
+    def verify_input_data_file(self):
+        alert = QtWidgets.QMessageBox()
+        alert.setText('The input filepath you selected is: \n{}'.format(self.fpath))
+        alert.exec_()
+        # alert.close()
+        # alert = None
+'''        
+
+'''
+    def onTextChanged(self,text):
+        settings = QtCore.QSettings()
+        settings.setValue("text", text)
+    def onClicked(self):
+        settings  =QtCore.QSettings()
+        text = settings.value("text")
+        print(text)
+''' 
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = uic.loadUi('MainWindow.ui')
+    window = MyTardisMetadataEditor()
     window.show()
-    # window.show()
     sys.exit(app.exec_())
