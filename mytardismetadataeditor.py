92151fbc7eb4e226b3a6062e4daab4d116f5a39e
Libby Li <libbyjuan69@gmail.com>
2022-06-19 22:16:34 +1200

    enable 'add files' and display

--- mytardismetadataeditor.py
+++ mytardismetadataeditor.py
@@ -1,10 +1,10 @@
 from fileinput import filename
 from os import fpathconf
-import sys
-<<<<<<< HEAD
-from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QWizard, QWizardPage, QVBoxLayout, QLabel,QFileDialog
+import os, sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QWizard, QTableWidget, QTableWidgetItem, QWizardPage, QVBoxLayout, QLabel,QFileDialog
 from PyQt5 import uic,QtWidgets,QtCore,QtGui
 # from mainwindow import Ui_MainWindow
+import models
 
 class MyTardisMetadataEditor(QMainWindow):
     def __init__(self):
@@ -14,14 +14,13 @@ class MyTardisMetadataEditor(QMainWindow):
         uic.loadUi('MainWindow.ui', self)
 
         # define our widgets
-        self.actionImport_data_files = self.findChild(QAction, "actionImport_data_files")
+        #self.actionImport_data_files = self.findChild(QAction, "actionImport_data_files")
         self.actionImport_data_files.triggered.connect(self.openWindow)
         self.show()
 
     def openWindow(self):  
         self.ui = WindowWizard()
         self.ui.show()
-###        self.close()
 
 class WindowWizard(QWizard):
     def __init__(self):
@@ -29,9 +28,9 @@ class WindowWizard(QWizard):
         uic.loadUi('add-files-wizard.ui', self)
 
         # define out widgets
-        self.datafilepushButton.clicked.connect(self.pushButton_handler)
+        self.datafilepushButton.clicked.connect(self.addFiles_handler)
 
-    def pushButton_handler(self):
+    def addFiles_handler(self):
         self.open_dialog_box()
 
     def fpath(self):
@@ -39,40 +38,34 @@ class WindowWizard(QWizard):
 
     def open_dialog_box(self):
         filename = QFileDialog.getOpenFileName()
-        path = filename[0]
-        self._fpath = path
-'''
-    def verify_input_data_file(self):
-        alert = QtWidgets.QMessageBox()
-        alert.setText('The input filepath you selected is: \n{}'.format(self.fpath))
-        alert.exec_()
-        # alert.close()
-        # alert = None
-'''        
+        fpath = filename[0]
+        self._fpath = fpath
+        def convert_bytes(size):
+            for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+                if size < 1024.0:
+                    return "%3.1f %s" % (size, x)
+                size /= 1024.0
+        if filename !="":
+            info = QtCore.QFileInfo(self._fpath)
+            size = info.size()
+            fname = info.fileName()
+            size_converted = convert_bytes(size)
+        newrow = [size_converted, fname]
+        table = self.datafiletableWidget
+        self.addTableRow(table,newrow)
+    
+    def addTableRow(self,table,row_data):
+        row = table.rowCount()
+        table.setRowCount(row+1)
+        col = 0
+        for item in row_data:
+            cell = QTableWidgetItem(str(item))
+            table.setItem(row, col, cell)
+            col += 1
 
-'''
-    def onTextChanged(self,text):
-        settings = QtCore.QSettings()
-        settings.setValue("text", text)
-    def onClicked(self):
-        settings  =QtCore.QSettings()
-        text = settings.value("text")
-        print(text)
-''' 
 
-=======
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWizard, QWizardPage, QVBoxLayout, QLabel
-from PyQt5 import uic
-from mainwindow import Ui_MainWindow
-from models import IngestionMetadata
 
-class MyTardisMetadataEditor(QMainWindow):
-    def __init__(self):
-        # Initialise the app ingestion metadata state.
-        self.metadata = IngestionMetadata()
-        QMainWindow.__init__(self)
 
->>>>>>> 49e064d2ecffead54c07436d560d234329dd3945
 if __name__ == "__main__":
     app = QApplication([])
     window = MyTardisMetadataEditor()
