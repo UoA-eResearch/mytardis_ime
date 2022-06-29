e9124fc80d5f4ac5070430fa819f7638b94d3020
Libby Li <libbyjuan69@gmail.com>
2022-06-29 15:55:12 +1200

    added dataset/experiment/project sizes

--- mytardismetadataeditor.py
+++ mytardismetadataeditor.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import QPersistentModelIndex,QModelIndex
 from isort import file
 import yaml
 # from mainwindow import Ui_MainWindow
-import models
+from models import IngestionMetadata, Project, Experiment, Dataset, Datafile, FileInfo
 
 class MyTardisMetadataEditor(QMainWindow):
     def __init__(self):
@@ -13,14 +13,13 @@ class MyTardisMetadataEditor(QMainWindow):
 
         # load the ui file
         uic.loadUi('MainWindow.ui', self)
-        self.metadata = models.IngestionMetadata()
+        self.metadata = IngestionMetadata()
 
         # define our widgets
         self.actionImport_data_files.triggered.connect(self.openWizardWindow)
         self.actionSave.triggered.connect(self.save_to_yaml)
         self.show()
 
-    # Need to modify to enable file/dataset... sizes displaying
     @QtCore.pyqtSlot('PyQt_PyObject','PyQt_PyObject','PyQt_PyObject','PyQt_PyObject')
     def reFresh(self,project_info, experiment_info, dataset_info, datafile_info):
         self.project,self.experiment,self.dataset,self.datafile = project_info, experiment_info, dataset_info, datafile_info 
@@ -29,12 +28,14 @@ class MyTardisMetadataEditor(QMainWindow):
         self.metadata.datasets.append(dataset_info)
         self.metadata.datafiles.append(datafile_info)
 
-        l1 = QTreeWidgetItem([self.dataset.dataset_name,"1",self.experiment.experiment_name])
-        l2 = QTreeWidgetItem([self.experiment.experiment_name,"3",self.project.project_name])
-        l3 = QTreeWidgetItem([self.project.project_name,"4"])
+        l1 = QTreeWidgetItem([self.dataset.dataset_name,self.dataset.metadata['Size'],self.experiment.experiment_name])
+        l2 = QTreeWidgetItem([self.experiment.experiment_name,self.experiment.metadata['Size'],self.project.project_name])
+        l3 = QTreeWidgetItem([self.project.project_name,self.project.metadata['Size']])
         
         for file in self.datafile.files:
-            l1_child = QTreeWidgetItem([file,"",""])
+            file_name = file.name
+            file_size = file.metadata['Size']
+            l1_child = QTreeWidgetItem([file_name,file_size,""])
             l1.addChild(l1_child)
         self.datasetTreeWidget.addTopLevelItem(l1)
         self.experimentTreeWidget.addTopLevelItem(l2)
@@ -81,24 +82,27 @@ class WindowWizard(QWizard):
     def fpath(self):
         return self._fpath
 
+    # calculate sizes of added datafiles in bytes,KB,MB,GB,TB
+    def convert_bytes(self,size):
+        for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+            if size < 1024.0:
+                return "%3.1f %s" % (size, x)
+            size /= 1024.0
+
     def open_dialog_box(self):
         filename_mode = QFileDialog()
         filename_mode.setFileMode(QFileDialog.ExistingFiles)
         filename = filename_mode.getOpenFileNames(self, "Open files")  
         fpath = filename[0]
         self._fpath = fpath
-        def convert_bytes(size):
-            for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
-                if size < 1024.0:
-                    return "%3.1f %s" % (size, x)
-                size /= 1024.0
+
         newrows = []
         for f in self._fpath:
             if f !="":
                 info = QtCore.QFileInfo(f)
                 size = info.size()
                 fname = info.fileName()
-                size_converted = convert_bytes(size)
+                size_converted = self.convert_bytes(size)
             newrow = [fname,size_converted, f]
             if newrow not in newrows:
                 newrows.append(newrow)
@@ -117,30 +121,53 @@ class WindowWizard(QWizard):
                 col += 1
     
     def on_submit(self):
-        project_info = models.Project()
-        experiment_info = models.Experiment()
-        dataset_info = models.Dataset()
-        datafile_info = models.Datafile()
+        project_info = Project()
+        experiment_info = Experiment()
+        dataset_info = Dataset()
+        datafile_info = Datafile()
 
         project_info.project_name = self.projectNameLineEdit.text()
         project_info.project_id = self.projectIDLineEdit.text()
         project_info.description = self.projectDescriptionLineEdit.text()
         experiment_info.experiment_name = self.experimentNameLineEdit.text()
         experiment_info.experiment_id = self.experimentIDLineEdit.text()
+        experiment_info.project_id = self.projectIDLineEdit.text()
         experiment_info.description = self.experimentdescriptionLineEdit.text()
         dataset_info.dataset_name = self.datasetNameLineEdit.text()
         dataset_info.dataset_id = self.instrumentIDLineEdit.text()
+        dataset_info.experiment_id = self.experimentIDLineEdit.text()
 
         table = self.datafiletableWidget
-        file_list = []
+        ### Calculate the size of dataset, experiment, project - need to modify if "Add data into existing Projects/experiment/datasets" enabled
+        B, K, M, G = [], [], [], []
         for row in range(table.rowCount()):
             file_name = table.item(row,0).text()
-            if file_name not in file_list:
-                file_list.append(file_name)
-            else:
-                continue
+            size = table.item(row,1).text()
+            file_info = FileInfo(name = file_name)
+            file_info.metadata['Size'] = size
+            datafile_info.files.append(file_info)
+            
+            l = len(size)
+            if size[l-2:] == "bytes":
+                B.append(float(size[:l-2]))
+            elif size[l-2:] == "KB":
+                K.append(float(size[:l-2]))
+            elif size[l-2:] == "MB":
+                M.append(float(size[:l-2]))
+            elif size[l-2:] == "GB":
+                G.append(float(size[:l-2]))
+        for i in K:
+            K[K.index(i)] = i * 1000
+        for i in M:
+            M[M.index(i)] = i * 1000000
+        for i in G: # to convert GigaBytes numbers to KiloBytes
+            G[G.index(i)] = i * 1000000000
+        size_sum_b = sum(B+K+M+G)
+        size_sum = self.convert_bytes(size_sum_b)
 
-        datafile_info.files = file_list
+        dataset_info.metadata['Size'] = size_sum
+        experiment_info.metadata['Size'] = size_sum
+        project_info.metadata['Size'] = size_sum
 
         self.submitted.emit(project_info, experiment_info, dataset_info, datafile_info)
         self.close()
