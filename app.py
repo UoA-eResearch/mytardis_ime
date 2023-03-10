f8bff4d81724f6f9ddd5babc4df9b8b3f7d4fbd5
LibbyLi667 <libbyjuan69@gmail.com>
2023-03-10 16:02:13 +1300

    can be ignored

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from ime.mytardismetadataeditor import MyTardisMetadataEditor
 import sys
 
 if __name__ == "__main__":
+    """
+    The main function that initializes and runs the MyTardis Metadata Editor GUI application.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     app = QApplication([])
     window = MyTardisMetadataEditor()
     window.show()
