2dabbe5c34c8f1752fef0a1a54faab70d338dfb5
LibbyLi667 <libbyjuan69@gmail.com>
2024-06-19 16:24:14 +1200

    fix pytest errors

--- ime/tests/test_image_parser.py
+++ ime/tests/test_image_parser.py
@@ -1,17 +1,12 @@
 from ime.parser.parsers import MetadataExtractor, extract_metadata, flatten_dict_keys_unique_id
 from ime.parser.image_parser import ImageProcessor
-from pathlib import Path
-import logging
 
-import jpype
-import scyjava
 import jpype.imports
 
 def test_get_metadata():
     from ime.parser.image_parser import ImageProcessor
     file_name = 'ime/tests/testdata/good image 02.oib'
     image_processor = ImageProcessor()
-    xml_string = image_processor.get_omexml_metadata(file_name)
     metadata = image_processor.get_metadata(file_name)
-    assert metadata['Instrument|Detector|Detector:0:0|Gain'] == '1000.0'
-    assert metadata['Instrument|Detector|Detector:0:0|Type'] == 'PMT'
+    assert metadata['Instrument|Detector|Detector:0:0|ID'] == 'Detector:0:0'
+    assert metadata['Instrument|Objective|LensNA'] == '1.35'
