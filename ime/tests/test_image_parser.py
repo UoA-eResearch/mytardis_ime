c7ed66ef0649047489b422890360d7dfed68bd7e
Chris Seal <c.seal@auckland.ac.nz>
2024-06-19 17:23:37 +1200

    Downgrade java VM version

--- ime/tests/test_image_parser.py
+++ ime/tests/test_image_parser.py
@@ -7,6 +7,6 @@ from ime.parser.image_parser import ImageProcessor
 def test_get_metadata():
     file_name = "ime/tests/testdata/good image 02.oib"
     image_processor = ImageProcessor()
-    # metadata = image_processor.get_metadata(file_name)
+    metadata = image_processor.get_metadata(file_name)
     assert metadata["Instrument|Detector|Detector:0:0|Gain"] == "1000.0"
     assert metadata["Instrument|Detector|Detector:0:0|Type"] == "PMT"
